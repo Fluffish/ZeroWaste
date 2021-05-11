508bf6129e6f286f97f2445dba4ca8193d39e039
Amel Boudraa <amel.boudraa@etu.univ-amu.fr>
2021-05-11 05:49:26 +0200

    test (erreur a cause des nv modules )

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,34 @@
-package com.company;
+package src.com.company;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.print.Collation;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.Scanner;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+
+        Parent root = FXMLLoader.load(getClass().getResource("test.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
 
     public static void main(String[] args) {
-	System.out.println("Hey!");
+        launch(args);
     }
 }
