496a0bdf9da74128a22804472700bf1614085f45
Amel Boudraa <amel.boudraa@etu.univ-amu.fr>
2021-05-16 23:53:08 +0200

    liaisons faites

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,31 +2,19 @@ package src.com.company;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.print.Collation;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
-
-import java.net.URL;
-import java.util.Scanner;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-
-        Parent root = FXMLLoader.load(getClass().getResource("profil.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("homepage.fxml"));
         primaryStage.setScene(new Scene(root, 700, 400));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
